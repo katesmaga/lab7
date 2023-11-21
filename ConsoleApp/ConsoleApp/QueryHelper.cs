16afb18280de29178405cb46f4b1edd864c5bec0
katesmaga <151572421+katesmaga@users.noreply.github.com>
2023-11-21 23:07:11 +0200

    Update QueryHelper.cs

--- ConsoleApp/ConsoleApp/QueryHelper.cs
+++ ConsoleApp/ConsoleApp/QueryHelper.cs
@@ -9,17 +9,34 @@ public class QueryHelper : IQueryHelper
     /// <summary>
     /// Get Deliveries that has payed
     /// </summary>
-    public IEnumerable<Delivery> Paid(IEnumerable<Delivery> deliveries) => new List<Delivery>(); //TODO: Завдання 1
+    public IEnumerable<Delivery> Paid(IEnumerable<Delivery> deliveries) => 
+        deliveries.Where(delivery => delivery.Status == DeliveryStatus.Confirmed);
 
     /// <summary>
     /// Get Deliveries that now processing by system (not Canceled or Done)
     /// </summary>
-    public IEnumerable<Delivery> NotFinished(IEnumerable<Delivery> deliveries) => new List<Delivery>(); //TODO: Завдання 2
     
+    public IEnumerable<Delivery> NotFinished(IEnumerable<Delivery> deliveries) =>
+        deliveries.Where(delivery => delivery.Status != DeliveryStatus.Cancelled && delivery.Status != DeliveryStatus.Done);
+
     /// <summary>
     /// Get DeliveriesShortInfo from deliveries of specified client
     /// </summary>
-    public IEnumerable<DeliveryShortInfo> DeliveryInfosByClient(IEnumerable<Delivery> deliveries, string clientId) => new List<DeliveryShortInfo>(); //TODO: Завдання 3
+    public IEnumerable<DeliveryShortInfo> DeliveryInfosByClient(IEnumerable<Delivery> deliveries, string clientId) =>
+        deliveries
+            .Where(delivery => delivery.ClientId == clientId)
+            .Select(delivery => new DeliveryShortInfo
+            {
+                Id = delivery.Id,
+                StartCity = delivery.Direction.Origin,
+                EndCity = delivery.Direction.Destination,
+                ClientId = delivery.ClientId,
+                Type = delivery.Type,
+                LoadingPeriod = delivery.LoadingPeriod,
+                ArrivalPeriod = delivery.ArrivalPeriod,
+                Status = delivery.Status,
+                CargoType = delivery.CargoType
+            });
     
     /// <summary>
     /// Get first ten Deliveries that starts at specified city and have specified type
@@ -29,29 +46,63 @@ public class QueryHelper : IQueryHelper
     /// <summary>
     /// Order deliveries by status, then by start of loading period
     /// </summary>
-    public IEnumerable<Delivery> OrderByStatusThenByStartLoading(IEnumerable<Delivery> deliveries) => new List<Delivery>();//TODO: Завдання 5
+    
+    public IEnumerable<Delivery> OrderByStatusThenByStartLoading(IEnumerable<Delivery> deliveries) =>
+        deliveries
+            .OrderBy(delivery => delivery.Status)
+            .ThenBy(delivery => delivery.LoadingPeriod);
 
     /// <summary>
     /// Count unique cargo types
     /// </summary>
-    public int CountUniqCargoTypes(IEnumerable<Delivery> deliveries) => 0; //TODO: Завдання 6
+    public int CountUniqCargoTypes(IEnumerable<Delivery> deliveries) =>
+        deliveries.Select(delivery => delivery.CargoType).Distinct().Count();
     
     /// <summary>
     /// Group deliveries by status and count deliveries in each group
     /// </summary>
-    public Dictionary<DeliveryStatus, int> CountsByDeliveryStatus(IEnumerable<Delivery> deliveries) => new();//TODO: Завдання 7
+    public Dictionary<DeliveryStatus, int> CountsByDeliveryStatus(IEnumerable<Delivery> deliveries) =>
+        deliveries
+            .GroupBy(delivery => delivery.Status)
+            .ToDictionary(group => group.Key, group => group.Count());
     
     /// <summary>
     /// Group deliveries by start-end city pairs and calculate average gap between end of loading period and start of arrival period (calculate in minutes)
     /// </summary>
-    public IEnumerable<AverageGapsInfo> AverageTravelTimePerDirection(IEnumerable<Delivery> deliveries) => new List<AverageGapsInfo>();//TODO: Завдання 8
+    public IEnumerable<AverageGapsInfo> AverageTravelTimePerDirection(IEnumerable<Delivery> deliveries) =>
+        deliveries.GroupBy(delivery => new { delivery.Direction.Origin, delivery.Direction.Destination })
+            .Select(group => new AverageGapsInfo
+            {
+                StartCity = group.Key.Origin,
+                EndCity = group.Key.Destination,
+                AverageGap = group.Average(delivery =>
+                {
+                    DateTime start = delivery.LoadingPeriod.Start.GetValueOrDefault();
+                    DateTime end = delivery.ArrivalPeriod.End.GetValueOrDefault();
 
+                    if (start != default && end != default)
+                    {
+                        return (end - start).TotalMinutes;
+                    }
+
+                    return 0.0; 
+                })
+            });
+    
     /// <summary>
     /// Paging helper
     /// </summary>
-    public IEnumerable<TElement> Paging<TElement, TOrderingKey>(IEnumerable<TElement> elements,
+   public IEnumerable<TElement> Paging<TElement, TOrderingKey>(IEnumerable<TElement> elements,
         Func<TElement, TOrderingKey> ordering,
         Func<TElement, bool>? filter = null,
         int countOnPage = 100,
-        int pageNumber = 1) => new List<TElement>(); //TODO: Завдання 9 
+        int pageNumber = 1)
+    {
+        var filteredElements = filter != null ? elements.Where(filter) : elements;
+
+        return filteredElements
+            .OrderBy(ordering)
+            .Skip((pageNumber - 1) * countOnPage)
+            .Take(countOnPage);
+    }
 }
